33bdd78a2797bea3f336ef26afaadba8ea0fd7b8
Francisco <fcovillegast@gmail.com>
2024-06-18 19:42:14 -0400

    Initial commit

---
+++ src/main/java/rrhh/cl/spring_cloud_server/SpringCloudServerApplication.java
@@ -0,0 +1,15 @@
+package rrhh.cl.spring_cloud_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/rrhh/cl/spring_cloud_server/configurations/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package rrhh.cl.spring_cloud_server.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration {
+
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername(username)
+                .password(bCryptPasswordEncoder.encode(password))
+                .roles("USER")
+                .build());
+        /*
+        manager.createUser(User.withUsername("admin")
+                .password(bCryptPasswordEncoder.encode("adminPass"))
+                .roles("USER", "ADMIN")
+                .build());
+                */
+
+        return manager;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                                //.requestMatchers("/**").hasAnyRole("ADMIN")
+                                .requestMatchers("/**").hasAnyRole("USER")
+                                .requestMatchers("/actuator/**").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+}
